9d4d864a3fa45eccd627d55bca64ecd7c961641a
Joahn Aml <joahn_aml@outlook.com>
2023-06-22 06:46:19 +0200

    feat: add manifest and service worker files

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '@fontsource/roboto';
+</script>
+
+<slot />
+
+<style>
+	:global(html) {
+		font-family: 'Roboto', sans-serif;
+	}
+
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { version } from '$lib/version.js';
+</script>
+
+<main>
+	<div>Hello {version}</div>
+</main>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+
+	main {
+		padding: 20px;
+	}
+</style>
